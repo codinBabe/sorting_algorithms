d02e14528eb0d87b6bd3e7c4e2d81109f2b7e75c
codinBabe <teboola2@gmail.com>
2023-10-22 05:57:50 -0700

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,1 +1,24 @@
 #include "sort.h"
+/**
+ * bubble_sort - bubble sort method of alogrithim
+ * @array: the array to sort
+ * @size: size of @array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j, temp;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+}
