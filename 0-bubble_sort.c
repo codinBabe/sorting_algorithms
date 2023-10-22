7cd560ba1a596bd7fc363843e7c5b5a88a51e774
codinBabe <teboola2@gmail.com>
2023-10-22 06:35:10 -0700

    modify bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -20,10 +20,10 @@ void bubble_sort(int *array, size_t size)
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
 				flag = 1;
+				print_array(array, size);
 			}
 		}
 		if (flag == 0)
 			break;
-		print_array(array, size);
 	}
 }
